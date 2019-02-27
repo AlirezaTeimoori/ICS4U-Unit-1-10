90be705a379500ddb761f77922c46d477cbb32c5
Alireza Teimoori <s445969@ocsb.local>
2019-02-27 14:24:34 -0500

    fix: code created and works

--- enumIntro.java
+++ enumIntro.java
@@ -11,19 +11,32 @@
 //-- how to make very basic use of enum--
 //----------------------------------------
 
+/*
+
+One of the reasons we use enum over array is that enum is a data type and 
+you can call the parameters by their names and even their belongings
+for example you can call the color of a dog under the type of animals
+but in arrays you can only call elements by their positions
+
+enums are constant. you need to know all the posible values because
+they will be fixed.
+
+*/
+
 import java.lang.Enum;
-
-
 
 public class enumIntro {
 
     public static void main(String[] args) {
-        for (int number : SolarSystem.values()){
-            System.out.println(SolarSystem.Earth.position);}
+        for (SolarSystem element : SolarSystem.values()){ // for each element
+            // Print the name of the name of the planet and the position
+            System.out.println(element + "\t==> planet number:  " + (element.position));
+        }
     }
 
+    public enum SolarSystem { // Intro data type
 
-    public enum SolarSystem {
+        // Intro elements and parameters
         Mercury(1),
         Venus(2),
         Earth(3),
@@ -33,12 +46,14 @@ public class enumIntro {
         Uranus(7),
         Neptune(8);
         
+        // Intro the position variable
         private final int position;
     
-        private SolarSystem (int position){
+        private SolarSystem (int position){ // Gives out the position
             this.position = position;
         }
             
         
     }
 }
+
